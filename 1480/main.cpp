a9bd88b213f861475976af9f0c81beb2e6e57770
Jessica Wang <wangjess@umich.edu>
2026-03-05 19:06:19 -0500

    Using std::

--- 1480/main.cpp
+++ 1480/main.cpp
@@ -1,12 +1,10 @@
 // https://leetcode.com/problems/running-sum-of-1d-array/
 #include <iostream>
 
-using namespace std;
-
-vector<int> runningSum(vector<int>& nums) {
+std::vector<int> runningSum(std::vector<int>& nums) {
 	int size = nums.size();
 
-	vector<int> result;
+	std::vector<int> result;
 	result.reserve(size);
 
 	int sum = 0;
@@ -22,19 +20,19 @@ int main() {
 	std::cout << "Init..." << std::endl;
 
 	// Example 1
-	vector<int> v1 = {1, 2, 3, 4};
-	vector<int> v1_sol = {1, 3, 6, 10};
+	std::vector<int> v1 = {1, 2, 3, 4};
+	std::vector<int> v1_sol = {1, 3, 6, 10};
 
 	// Example 2
-	vector<int> v2 = {1, 1, 1, 1, 1};
-	vector<int> v2_sol = {1, 2, 3, 4, 5};
+	std::vector<int> v2 = {1, 1, 1, 1, 1};
+	std::vector<int> v2_sol = {1, 2, 3, 4, 5};
 
 	// Example 3
-	vector<int> v3 = {3, 1, 2, 10, 1};
-	vector<int> v3_sol = {3, 4, 6, 16, 17};
+	std::vector<int> v3 = {3, 1, 2, 10, 1};
+	std::vector<int> v3_sol = {3, 4, 6, 16, 17};
 
 	// Testing...
-	vector<int> v1_m = runningSum(v1);
+	std::vector<int> v1_m = runningSum(v1);
 	if (v1_m != v1_sol) {
 		std::cerr << "Test_1 failed\n";	
 		return 1;
@@ -42,7 +40,7 @@ int main() {
 		std::cout << "Equal\n";
 	}
 
-	vector<int> v2_m = runningSum(v2);
+	std::vector<int> v2_m = runningSum(v2);
 	if (v2_m != v2_sol) {
 		std::cerr << "Test_2 failed\n";	
 		return 1;
@@ -50,7 +48,7 @@ int main() {
 		std::cout << "Equal\n";
 	}
 
-	vector<int> v3_m = runningSum(v3);
+	std::vector<int> v3_m = runningSum(v3);
 	if (v3_m != v3_sol) {
 		std::cerr << "Test_3 failed\n";	
 		return 1;

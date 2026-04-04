4e241c459d2c42b3a337ab37a90f76d48f8c5f18
Jessica Wang <wangjess@umich.edu>
2026-04-03 23:54:20 -0400

    Compacted code

--- medium/334/main.cpp
+++ medium/334/main.cpp
@@ -13,15 +13,9 @@ bool increasingTriplet(vector<int>& nums) {
     int min1 = INT_MAX, min2 = INT_MAX;
 
     for (int n : nums) {
-        if (n <= min1) {
-            min1 = n;
-        }
-        else if (n <= min2) {
-            min2 = n;
-        }
-        else {
-            return true;
-        }
+        if (n <= min1) min1 = n;
+        else if (n <= min2) min2 = n;
+        else return true;
     }
 
     return false;

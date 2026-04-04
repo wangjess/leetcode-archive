c7f75f89c4ed9d82f655245db52821b0588c5831
Jessica Wang <wangjess@umich.edu>
2026-04-04 17:02:25 -0400

    Completed an interesting String Compression problem

--- medium/443/main.cpp
+++ medium/443/main.cpp
@@ -1,8 +1,27 @@
 //https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
 
 #include <vector>
+#include <cassert>
 
 using namespace std;
+
+// O(n) time -> read visits each character once, picks up where left off
+// O(1) space
+int compress(vector<char>& chars) {
+    int write = 0, n = chars.size();
+
+    for (int read = 0; read < n; ) {
+        char curr = chars[read];
+        int streak = 0;
+        while (read < n && chars[read] == curr) { read++; streak++; }
+        chars[write++] = curr;
+        if (streak > 1) {
+            for (char c : to_string(streak)) chars[write++] = c; 
+        }
+    }
+
+    return write;
+}
 
 int main() {
     // Example 1
@@ -14,7 +33,13 @@ int main() {
     // Example 3
     vector<char> v3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
 
-    
+    int v1_res = compress(v1);
+    int v2_res = compress(v2);
+    int v3_res = compress(v3);
+
+    assert(v1_res == 6);
+    assert(v2_res == 1);
+    assert(v3_res == 4);
 
     return 0;
 }

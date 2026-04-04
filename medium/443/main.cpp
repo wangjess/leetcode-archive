e7e26af3ebd4998aa936dbff73b242235d7cf87b
Jessica Wang <wangjess@umich.edu>
2026-04-04 00:00:17 -0400

    Beginning a new problem

---
+++ medium/443/main.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Example 1
+    vector<char> v1 = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+
+    // Example 2
+    vector<char> v2 = {'a'};
+
+    // Example 3
+    vector<char> v3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+
+    
+
+    return 0;
+}
